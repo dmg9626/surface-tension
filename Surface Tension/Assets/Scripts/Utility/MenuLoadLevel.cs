5d5d175ea4c4096faa08a1e257d37b576ff70cc7
Jacob Dixey <jmd585@drexel.edu>
2018-06-13 23:42:14 -0400

    Created Working Main Menu and scripts necessary. Put it into Prefab.

---
+++ Surface Tension/Assets/Scripts/Utility/MenuLoadLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuLoadLevel : MonoBehaviour {
+
+    public void sceneChange(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+}
