e63a4af537d6811caf3058658c8cea7b590e46c9
Jacob Dixey <jmd585@drexel.edu>
2018-06-14 17:29:04 -0400

    Escape now takes player back to main menu, got rid of extra audio controller in level 1

--- Surface Tension/Assets/Scripts/Player/Player.cs
+++ Surface Tension/Assets/Scripts/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Player : MonoBehaviour 
@@ -897,7 +898,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Quit"))
         {
-            Application.Quit();
+            SceneManager.LoadScene("Level 0");
         }
     }
 }
